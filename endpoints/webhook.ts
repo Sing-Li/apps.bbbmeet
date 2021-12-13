6d0738a117cb23d98505e4fd4f28a9555943d880
yash-rajpal <rajpal.yash03@gmail.com>
2021-12-13 13:50:06 +0530

    ToDo app cleanup

--- endpoints/webhook.ts
+++ endpoints/webhook.ts
@@ -1,5 +1,7 @@
-import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { IHttp, IMessageBuilder, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { ApiEndpoint, IApiEndpointInfo, IApiRequest, IApiResponse } from '@rocket.chat/apps-engine/definition/api';
+import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
+import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
 
 export class WebhookEndpoint extends ApiEndpoint {
     public path = 'webhook';
@@ -13,7 +15,7 @@ export class WebhookEndpoint extends ApiEndpoint {
         persis: IPersistence,
     ): Promise<IApiResponse> {
 
-        console.log("Test Endpoint")
+        console.log("Test Endpoint");
 
         return this.success();
     }
